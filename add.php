3cd5089b90ebf1f6e165d31c050f52fdd1f72b2a
Problem <pboyd04@gmail.com>
2015-02-15 21:55:08 +0000

    Finish add page

--- add.php
+++ add.php
@@ -9,13 +9,17 @@ $db = new RegistrationDB();
 $user = FlipSession::get_user();
 if($user)
 {
-    $camps = $db->getAllThemeCampsForUser($user->uid[0]);
-    $arts  = $db->getAllArtProjectsForUser($user->uid[0]);
+    $camps  = $db->getAllThemeCampsForUser($user->uid[0]);
+    $arts   = $db->getAllArtProjectsForUser($user->uid[0]);
+    $cars   = $db->getAllArtCarsForUser($user->uid[0]);
+    $events = $db->getAllEventsForUser($user->uid[0]);
 }
 else
 {
-    $camps = array();
-    $arts  = array();
+    $camps  = array();
+    $arts   = array();
+    $cars   = array();
+    $events = array();
 }
 
 $manage_camp = '<li><a href="tc_reg.php">Register a theme camp</a></li>';
@@ -28,6 +32,26 @@ if(count($arts) > 0)
 {
     $manage_art = '<li><a href="art_reg.php?_id='.$arts[0]['_id'].'">Manage your art project</a></li>';
 }
+$manage_car = '<li><a href="artCar_reg.php">Register an art car</a></li>';
+if(count($cars))
+{
+    $manage_car = '<li><a href="artCar_reg.php?_id='.$cars[0]['_id'].'">Manage your art car</a></li>';
+}
+$manage_event = '<li><a href="event_reg.php">Register a new event</a></li>';
+$event_count = count($events);
+if($event_count == 1)
+{
+    $manage_event.= '<li><a href="event_reg.php?_id='.$events[0]['_id'].'">Manage your event</a></li>';
+}
+else if($event_count >= 1)
+{
+    $manage_event.= '<ul>';
+    for($i = 0; $i < $event_count; $i++)
+    {
+       $manage_event.= '<li><a href="event_reg.php?_id='.$events[$i]['_id'].'">Manage your event: '.$events[$i]['name'].'</a></li>'; 
+    }
+    $manage_event.= '</ul>'; 
+}
 
 $page->body .= '
 <div id="content">
@@ -37,8 +61,8 @@ $page->body .= '
     <ul>
         '.$manage_camp.'
         '.$manage_art.'
-        <li><a href="artCar_reg.php">Register an art car</a></li>
-        <li><a href="event_reg.php">Register an event</a></li>
+        '.$manage_car.'
+        '.$manage_event.'
     </ul>
 </div>';
 
