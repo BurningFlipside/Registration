dfcd7acd893ac561e075c151fc01d62e11c3330f
Problem <pboyd04@gmail.com>
2015-09-02 15:28:32 +0000

    Remove unneeded RegistrationDB reference

--- _admin/index.php
+++ _admin/index.php
@@ -2,7 +2,6 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require_once('class.RegisterAdminPage.php');
-require_once('class.RegistrationDB.php');
 $page = new RegisterAdminPage('Burning Flipside - Tickets');
 
 $page->add_js_from_src('js/index.js');
