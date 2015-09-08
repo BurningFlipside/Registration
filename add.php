1669e462c95ec28f0834fe00fb314681731e3e7c
Problem <pboyd04@gmail.com>
2015-09-08 03:20:50 +0000

    Remove old RegistrationDB from add.php

--- add.php
+++ add.php
@@ -2,61 +2,71 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require_once('class.SecurePage.php');
-require_once('class.RegistrationDB.php');
 $page = new SecurePage('Burning Flipside - Registration');
 
-$db = new RegistrationDB();
-$user = FlipSession::get_user();
-if($user)
-{
-    $camps  = $db->getAllThemeCampsForUser($user->uid[0]);
-    $arts   = $db->getAllArtProjectsForUser($user->uid[0]);
-    $cars   = $db->getAllArtCarsForUser($user->uid[0]);
-    $events = $db->getAllEventsForUser($user->uid[0]);
-}
-else
-{
-    $camps  = array();
-    $arts   = array();
-    $cars   = array();
-    $events = array();
-}
+$camps  = array();
+$arts   = array();
+$cars   = array();
+$events = array();
 
-$manage_camp = '<li><a href="tc_reg.php">Register a theme camp</a></li>';
-if(count($camps) > 0)
+if($page->user)
 {
-    $manage_camp = '<li><a href="tc_reg.php?_id='.$camps[0]['_id'].'">Manage your theme camp</a></li>';
+    $data_set = DataSetFactory::get_data_set('registration');
+    $vars_data_table = $data_set['vars'];
+    $camps_data_table = $data_set['camps'];
+    $art_data_table = $data_set['art'];
+    $dmv_data_table = $data_set['dmv'];
+    $event_data_table = $data_set['event'];
+
+    $vars = $vars_data_table->read(new \Data\Filter('name eq year'));
+    $year = $vars[0]['value'];
+
+    $filter = array('year'=>$year, 'registrars'=>$page->user->getUid());
+
+    $camps  = $camps_data_table->read($filter);
+    $arts   = $art_data_table->read($filter);
+    $cars   = $dmv_data_table->read($filter);
+    $events = $event_data_table->read($filter);
+}
+$camps_count  = count($camps);
+$arts_count   = count($arts);
+$cars_count   = count($cars);
+$events_count = count($events);
+
+
+$manage_camp = '<li><a href="tc_reg.php">Register a new theme camp</a></li>';
+if($camps_count > 0)
+{
+    for($i = 0; $i < $camps_count; $i++)
+    {
+        $manage_camp.= '<li><a href="tc_reg.php?_id='.$camps[$i]['_id'].'">Manage your theme camp: '.$camps[$i]['name'].'</a></li>';
+    }
 }
 $manage_art = '<li><a href="art_reg.php">Register a new art project</a></li>';
-$art_count = count($arts);
-if($art_count == 1)
-{
-    $manage_art.= '<li><a href="art_reg.php?_id='.$arts[0]['_id'].'">Manage your art project</a></li>';
-}
-else if($art_count >= 1)
+if($arts_count > 0)
 {
     $manage_art.= '<ul>';
-    for($i = 0; $i < $art_count; $i++)
+    for($i = 0; $i < $arts_count; $i++)
     {
        $manage_art.= '<li><a href="art_reg.php?_id='.$arts[$i]['_id'].'">Manage your art project: '.$arts[$i]['name'].'</a></li>';
     }
     $manage_art.= '</ul>';
 }
-$manage_car = '<li><a href="artCar_reg.php">Register an art car</a></li>';
-if(count($cars))
+$manage_car = '<li><a href="artCar_reg.php">Register a new art car</a></li>';
+if($cars_count > 0)
 {
-    $manage_car = '<li><a href="artCar_reg.php?_id='.$cars[0]['_id'].'">Manage your art car</a></li>';
+    $manage_car.= '<ul>';
+    for($i = 0; $i < $events_count; $i++)
+    {
+        $manage_car = '<li><a href="artCar_reg.php?_id='.$cars[$i]['_id'].'">Manage your art car: '.$cars[$i]['name'].'</a></li>';
+    }
+    $manage_car.= '</ul>';
 }
 $manage_event = '<li><a href="event_reg.php">Register a new event</a></li>';
-$event_count = count($events);
-if($event_count == 1)
-{
-    $manage_event.= '<li><a href="event_reg.php?_id='.$events[0]['_id'].'">Manage your event</a></li>';
-}
-else if($event_count >= 1)
+if($events_count > 0)
 {
     $manage_event.= '<ul>';
-    for($i = 0; $i < $event_count; $i++)
+    for($i = 0; $i < $events_count; $i++)
     {
        $manage_event.= '<li><a href="event_reg.php?_id='.$events[$i]['_id'].'">Manage your event: '.$events[$i]['name'].'</a></li>'; 
     }
