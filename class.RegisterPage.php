f526d6dd5f61b101edac858bf4912faf2ed5d45c
Problem <pboyd04@gmail.com>
2016-01-07 19:26:33 +0000

    Fix issues from secure-error_log

--- class.RegisterPage.php
+++ class.RegisterPage.php
@@ -10,7 +10,7 @@ class RegisterPage extends SecurePage
         $root = $_SERVER['DOCUMENT_ROOT'];
         $script_dir = dirname(__FILE__);
         $this->register_root = substr($script_dir, strlen($root));
-        $user = FlipSession::get_user(true);
+        $user = FlipSession::getUser();
         if($user->isInGroupNamed('RegistrationAdmins') ||
            $user->isInGroupNamed('ArtAdmins')          ||
            $user->isInGroupNamed('CampAdmins')         ||
