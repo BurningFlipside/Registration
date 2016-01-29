30f501e32276aeb34d9be5c3f796449dddd7f723
Problem <pboyd04@gmail.com>
2016-01-29 22:09:19 +0000

    Toggle registrations based on date

--- add.php
+++ add.php
@@ -4,10 +4,14 @@ error_reporting(E_ALL);
 require_once('class.SecurePage.php');
 $page = new SecurePage('Burning Flipside - Registration');
 
-$camps  = array();
-$arts   = array();
-$cars   = array();
-$events = array();
+$camps    = array();
+$arts     = array();
+$cars     = array();
+$events   = array();
+$tcDates  = array();
+$artDates = array();
+$dmvDates = array();
+$evtDates = array();
 
 if($page->user)
 {
@@ -21,6 +25,18 @@ if($page->user)
     $vars = $vars_data_table->read(new \Data\Filter('name eq year'));
     $year = $vars[0]['value'];
 
+    $vars = $vars_data_table->read(new \Data\Filter('name eq tcRegDates'));
+    $tcDates = $vars[0]['value'];
+
+    $vars = $vars_data_table->read(new \Data\Filter('name eq artRegDates'));
+    $artDates = $vars[0]['value'];
+
+    $vars = $vars_data_table->read(new \Data\Filter('name eq dmvRegDates'));
+    $dmvDates = $vars[0]['value'];
+
+    $vars = $vars_data_table->read(new \Data\Filter('name eq eventRegDates'));
+    $evtDates = $vars[0]['value'];
+
     $filter = array('year'=>$year, 'registrars'=>$page->user->getUid());
 
     $camps  = $camps_data_table->read($filter);
@@ -33,42 +49,101 @@ $arts_count   = count($arts);
 $cars_count   = count($cars);
 $events_count = count($events);
 
+$now = getdate();
+$tcStart = date_parse($tcDates['start']);
+$tcEnd   = date_parse($tcDates['end']);
 
 $manage_camp = '<li><a href="tc_reg.php">Register a new theme camp</a></li>';
+if($now < $tcStart || $now > $tcEnd)
+{
+    $manage_camp = '<li>Theme Camp Registration is Closed</li>';
+}
 if($camps_count > 0)
 {
     for($i = 0; $i < $camps_count; $i++)
     {
-        $manage_camp.= '<li><a href="tc_reg.php?_id='.$camps[$i]['_id'].'">Manage your theme camp: '.$camps[$i]['name'].'</a></li>';
+        if($now < $tcStart || $now > $tcEnd)
+        {
+            $manage_camp.= '<li><a href="tc_reg.php?_id='.$camps[$i]['_id'].'">View your theme camp: '.$camps[$i]['name'].'</a></li>';
+        }
+        else
+        { 
+            $manage_camp.= '<li><a href="tc_reg.php?_id='.$camps[$i]['_id'].'">Manage your theme camp: '.$camps[$i]['name'].'</a></li>';
+        }
     }
 }
+
+$artStart = date_parse($artDates['start']);
+$artEnd   = date_parse($artDates['end']);
+
 $manage_art = '<li><a href="art_reg.php">Register a new art project</a></li>';
+if($now < $artStart || $now > $artEnd)
+{
+    $manage_art = '<li>Art Registration is Closed</li>';
+}
 if($arts_count > 0)
 {
     $manage_art.= '<ul>';
     for($i = 0; $i < $arts_count; $i++)
     {
-       $manage_art.= '<li><a href="art_reg.php?_id='.$arts[$i]['_id'].'">Manage your art project: '.$arts[$i]['name'].'</a></li>';
+       if($now < $artStart || $now > $artEnd)
+       {
+           $manage_art.= '<li><a href="art_reg.php?_id='.$arts[$i]['_id'].'">View your art project: '.$arts[$i]['name'].'</a></li>';
+       }
+       else
+       {
+           $manage_art.= '<li><a href="art_reg.php?_id='.$arts[$i]['_id'].'">Manage your art project: '.$arts[$i]['name'].'</a></li>';
+       }
     }
     $manage_art.= '</ul>';
 }
+
+$dmvStart = date_parse($dmvDates['start']);
+$dmvEnd   = date_parse($dmvDates['end']);
+
 $manage_car = '<li><a href="artCar_reg.php">Register a new art car</a></li>';
+if($now < $dmvStart || $now > $dmvEnd)
+{
+    $manage_car = '<li>Art Car Registration is Closed</li>';
+}
 if($cars_count > 0)
 {
     $manage_car.= '<ul>';
     for($i = 0; $i < $events_count; $i++)
     {
-        $manage_car = '<li><a href="artCar_reg.php?_id='.$cars[$i]['_id'].'">Manage your art car: '.$cars[$i]['name'].'</a></li>';
+        if($now < $dmvStart || $now > $dmvEnd)
+        {
+            $manage_car = '<li><a href="artCar_reg.php?_id='.$cars[$i]['_id'].'">View your art car: '.$cars[$i]['name'].'</a></li>';
+        }
+        else
+        {
+            $manage_car = '<li><a href="artCar_reg.php?_id='.$cars[$i]['_id'].'">Manage your art car: '.$cars[$i]['name'].'</a></li>';
+        }
     }
     $manage_car.= '</ul>';
 }
+
+$evtStart = date_parse($evtDates['start']);
+$evtEnd   = date_parse($evtDates['end']);
+
 $manage_event = '<li><a href="event_reg.php">Register a new event</a></li>';
+if($now < $evtStart || $now > $evtEnd)
+{
+    $manage_event = '<li>Event Registration is Closed</li>';
+}
 if($events_count > 0)
 {
     $manage_event.= '<ul>';
     for($i = 0; $i < $events_count; $i++)
     {
-       $manage_event.= '<li><a href="event_reg.php?_id='.$events[$i]['_id'].'">Manage your event: '.$events[$i]['name'].'</a></li>'; 
+        if($now < $evtStart || $now > $evtEnd)
+        {
+            $manage_event.= '<li><a href="event_reg.php?_id='.$events[$i]['_id'].'">View your event: '.$events[$i]['name'].'</a></li>';
+        }
+        else
+        {
+            $manage_event.= '<li><a href="event_reg.php?_id='.$events[$i]['_id'].'">Manage your event: '.$events[$i]['name'].'</a></li>'; 
+        }
     }
     $manage_event.= '</ul>'; 
 }
--- plugin.php
+++ plugin.php
@@ -3,11 +3,53 @@ class RegistrationPlugin extends SecurePlugin
 {
     function get_secure_menu_entries($page, $user)
     {
-        $ret = array('View Registrations'=>$page->secure_root.'register/view.php',
-                     'Theme Camp Registration'=>$page->secure_root.'register/tc_reg.php',
-                     'Art Project Registration'=>$page->secure_root.'register/art_reg.php',
-                     'Art Car Registration'=>$page->secure_root.'register/artCar_reg.php',
-                     'Event Registration'=>$page->secure_root.'register/event_reg.php');
+        $ret = array('View Registrations'=>$page->secure_root.'register/view.php');
+
+        $data_set = DataSetFactory::get_data_set('registration');
+        $vars_data_table = $data_set['vars'];
+
+        $vars = $vars_data_table->read(new \Data\Filter('name eq tcRegDates'));
+        $tcDates = $vars[0]['value'];
+
+        $vars = $vars_data_table->read(new \Data\Filter('name eq artRegDates'));
+        $artDates = $vars[0]['value'];
+
+        $vars = $vars_data_table->read(new \Data\Filter('name eq dmvRegDates'));
+        $dmvDates = $vars[0]['value'];
+
+        $vars = $vars_data_table->read(new \Data\Filter('name eq eventRegDates'));
+        $evtDates = $vars[0]['value'];
+
+        $now = getdate();
+        $tcStart  = date_parse($tcDates['start']);
+        $tcEnd    = date_parse($tcDates['end']);
+        $artStart = date_parse($artDates['start']);
+        $artEnd   = date_parse($artDates['end']);
+        $dmvStart = date_parse($dmvDates['start']);
+        $dmvEnd   = date_parse($dmvDates['end']);
+        $evtStart = date_parse($evtDates['start']);
+        $evtEnd   = date_parse($evtDates['end']);
+
+        if($now > $tcStart && $now < $tcEnd)
+        {
+            $ret['Theme Camp Registration']=$page->secure_root.'register/tc_reg.php';
+        }
+
+        if($now > $artStart && $now < $artEnd)
+        {
+            $ret['Art Project Registration']=$page->secure_root.'register/art_reg.php';
+        }
+
+        if($now > $dmvStart && $now < $dmvEnd)
+        {
+            $ret['Art Car Registration']=$page->secure_root.'register/artCar_reg.php';
+        }
+
+        if($now > $evtStart && $now < $evtEnd)
+        {
+            $ret['Event Registration']=$page->secure_root.'register/event_reg.php';
+        }
+
         if($user !== null && 
            ($user->isInGroupNamed('RegistrationAdmins') || $user->isInGroupNamed('ArtAdmins') || $user->isInGroupNamed('CampAdmins') || $user->isInGroupNamed('DMVAdmins') || $user->isInGroupNamed('EventAdmins')))
         {
