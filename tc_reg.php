fdfcab206ebb22040df3c0d9510187a3f1ad77c3
Problem <pboyd04@gmail.com>
2016-02-04 15:40:04 +0000

    Add common JS to TC reg form

--- tc_reg.php
+++ tc_reg.php
@@ -4,6 +4,7 @@ error_reporting(E_ALL);
 require_once('class.RegisterWizardPage.php');
 $page = new RegisterWizardPage('Theme Camp', 'Camp');
 
+$page->add_js_from_src('js/reg.js');
 $page->add_js_from_src('js/tc_reg.js');
 
 $index = $page->add_wizard_step('Basic Questions');
