a2473e4fe46c44aec61d9d1667cfd24b7a40854a
Problem <pboyd04@gmail.com>
2016-02-06 15:43:24 +0000

    Swtich from date APIs to time APIs for reg open

--- add.php
+++ add.php
@@ -49,9 +49,9 @@ $arts_count   = count($arts);
 $cars_count   = count($cars);
 $events_count = count($events);
 
-$now = getdate();
-$tcStart = date_parse($tcDates['start']);
-$tcEnd   = date_parse($tcDates['end']);
+$now = time();
+$tcStart = strtotime($tcDates['start']);
+$tcEnd   = strtotime($tcDates['end']);
 
 $manage_camp = '<li><a href="tc_reg.php">Register a new theme camp</a></li>';
 if($now < $tcStart || $now > $tcEnd)
@@ -73,8 +73,8 @@ if($camps_count > 0)
     }
 }
 
-$artStart = date_parse($artDates['start']);
-$artEnd   = date_parse($artDates['end']);
+$artStart = strtotime($artDates['start']);
+$artEnd   = strtotime($artDates['end']);
 
 $manage_art = '<li><a href="art_reg.php">Register a new art project</a></li>';
 if($now < $artStart || $now > $artEnd)
@@ -98,8 +98,8 @@ if($arts_count > 0)
     $manage_art.= '</ul>';
 }
 
-$dmvStart = date_parse($dmvDates['start']);
-$dmvEnd   = date_parse($dmvDates['end']);
+$dmvStart = strtotime($dmvDates['start']);
+$dmvEnd   = strtotime($dmvDates['end']);
 
 $manage_car = '<li><a href="artCar_reg.php">Register a new art car</a></li>';
 if($now < $dmvStart || $now > $dmvEnd)
@@ -123,8 +123,8 @@ if($cars_count > 0)
     $manage_car.= '</ul>';
 }
 
-$evtStart = date_parse($evtDates['start']);
-$evtEnd   = date_parse($evtDates['end']);
+$evtStart = strtotime($evtDates['start']);
+$evtEnd   = strtotime($evtDates['end']);
 
 $manage_event = '<li><a href="event_reg.php">Register a new event</a></li>';
 if($now < $evtStart || $now > $evtEnd)
