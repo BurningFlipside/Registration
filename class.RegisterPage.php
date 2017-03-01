56ac9e669feffca9896f5a9d2b06cf09b6902a48
Problem <pboyd04@gmail.com>
2017-03-01 15:49:04 +0000

    Centralize Login Code

--- class.RegisterPage.php
+++ class.RegisterPage.php
@@ -20,4 +20,18 @@ class RegisterPage extends SecurePage
            $this->addLink('Admin', $this->register_root.'/_admin/');
         }
     }
+
+    public function printPage($header = true)
+    {
+        if($this->user === false || $this->user === null)
+        {
+            $this->body = '
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">You must <a href="'.$this->loginUrl.'?return='.$this->currentUrl().'">log in <span class="glyphicon glyphicon-log-in"></span></a> to access the '.$this->title.' system!</h1>
+            </div>
+        </div>';
+        }
+        parent::printPage();
+    }
 }
