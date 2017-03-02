aed1d0cd5431462c9714ab001c6be69e2991bbb6
Problem <pboyd04@gmail.com>
2017-03-02 00:10:48 +0000

    Switch Registration to SecureLoginRequiredPage

--- class.RegisterPage.php
+++ class.RegisterPage.php
@@ -1,6 +1,6 @@
 <?php
-require_once('../class.SecurePage.php');
-class RegisterPage extends SecurePage
+require_once('../class.SecureLoginRequiredPage.php');
+class RegisterPage extends SecureLoginRequiredPage
 {
     public $register_root;
 
@@ -19,19 +19,5 @@ class RegisterPage extends SecurePage
         {
            $this->addLink('Admin', $this->register_root.'/_admin/');
         }
-    }
-
-    public function printPage($header = true)
-    {
-        if($this->user === false || $this->user === null)
-        {
-            $this->body = '
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">You must <a href="'.$this->loginUrl.'?return='.$this->currentUrl().'">log in <span class="glyphicon glyphicon-log-in"></span></a> to access the '.$this->title.' system!</h1>
-            </div>
-        </div>';
-        }
-        parent::printPage();
     }
 }
