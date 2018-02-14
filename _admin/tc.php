165294e9ede72af519c955567649590d9bf1e01c
Problem <pboyd04@gmail.com>
2018-02-14 02:46:45 +0000

    Update theme camp admin links

--- _admin/tc.php
+++ _admin/tc.php
@@ -23,12 +23,12 @@ else
             </div>
         </div>
         <div class="row">
-            <a href="../api/v1/camps?fmt=csv2&filter=year eq 2017&no_logo">Big Spreadsheet of Everything (.csv)</a> |
-            <a href="../api/v1/camps/*/campLead?fmt=csv&filter=year eq 2017">Camp Leads (.csv)</a> |
-            <a href="../api/v1/camps/*/soundLead?fmt=csv&filter=year eq 2017">Sound Leads (.csv)</a> |
-            <a href="../api/v1/camps/*/safetyLead?fmt=csv&filter=year eq 2017">Safety Leads (.csv)</a> |
-            <a href="../api/v1/camps/*/volunteering?fmt=csv&filter=year eq 2017">Volunteering Leads (.csv)</a> |
-            <a href="../api/v1/camps/*/cleanupLead?fmt=csv&filter=year eq 2017">Cleanup Leads (.csv)</a>
+            <a href="../api/v1/camps?fmt=csv&filter=year eq 2018&no_logo">Big Spreadsheet of Everything (.csv)</a> |
+            <a href="../api/v1/camps/*/campLead?fmt=csv&filter=year eq 2018">Camp Leads (.csv)</a> |
+            <a href="../api/v1/camps/*/soundLead?fmt=csv&filter=year eq 2018">Sound Leads (.csv)</a> |
+            <a href="../api/v1/camps/*/safetyLead?fmt=csv&filter=year eq 2018">Safety Leads (.csv)</a> |
+            <a href="../api/v1/camps/*/volunteering?fmt=csv&filter=year eq 2018">Volunteering Leads (.csv)</a> |
+            <a href="../api/v1/camps/*/cleanupLead?fmt=csv&filter=year eq 2018">Cleanup Leads (.csv)</a>
         </div>
         <div class="row">
             <table id="tc" class="table">
