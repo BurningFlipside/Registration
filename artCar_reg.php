7f98caffb843d9ffd59776102f2e49d355d198d9
Russell <russell.t.sherman@gmail.com>
2019-02-02 07:27:19 -0600

    car reg form

--- artCar_reg.php
+++ artCar_reg.php
@@ -4,29 +4,31 @@ error_reporting(E_ALL);
 require_once('class.RegisterWizardPage.php');
 $page = new RegisterWizardPage('Art Car');
 
+
 $index = $page->add_wizard_step('Basic Questions');
 $page->add_form_group($index, 'Vehicle Owner\'s Name', 'vehicle_owner', 'text', 'This is the person who will be attending the event and be responsible for: any and all actions of vehicle, driver and passengers of mutant vehicle; and liability of said vehicle.', array('required'=>'true'));
-
 $page->add_form_group($index, 'Email Address', 'vehicle_email', 'text', 'So we can get in touch with you.', array('required'=>'true'));
-
 $page->add_form_group($index, 'Phone Number', 'vehicle_phone', 'text', 'So we can get in touch with you.');
-
 $page->add_form_group($index, 'Theme Camp (if any)', 'vehicle_camp', 'text', 'So we can find you on site.');
-
 $page->add_form_group($index, 'Vehicle Dimensions', 'vehicle_dimention', 'text', 'Our roads are narrow and pass under pecan trees. Let us know the height, width and length of your vehicle.', array('required'=>'true'));
-
 $page->add_form_group($index, 'Number of Passengers', 'vehicle_passengers', 'text', 'The number of passengers you can comfortably carry.', array('required'=>'true'));
-
-$page->add_form_group($index, 'This art car has sound', 'has_sound', 'checkbox', 'This art car will utilize amplified sound in some form.', array('class'=>'ignore', 'data-tabcontrol'=>'sound'));
-
-$page->add_form_group($index, 'This art car has flame effects', 'has_fe', 'checkbox', 'This art car has flame effects.', array('class'=>'ignore', 'data-tabcontrol'=>'fe'));
+$page->add_raw_html($index, '<div class="form-check">
+    <input class="form-check-input" type="checkbox" name="has_sound" id="has_sound"
+        data-toggle="tooltip" data-placement="top" title="" 
+        data-original-title="This art car will utilize amplified sound in some form.">
+    <label for="has_sound" class="form-check-label non-required">This art car has sound</label>
+</div>');
+$page->add_raw_html($index, '<div class="form-check">
+    <input class="form-check-input" type="checkbox" name="has_fe" id="has_fe"
+        data-toggle="tooltip" data-placement="top" title="" 
+        data-original-title="This art car has flame effects.">
+    <label for="has_fe" class="form-check-label non-required">This art car has flame effects</label>
+</div>');
 
 
 $index = $page->add_wizard_step('Images');
 $page->add_form_group($index, 'Image #1', 'image_1', 'file', 'A picture or drawing of your art car.', array('required'=>'true'));
-
 $page->add_form_group($index, 'Image #2', 'image_2', 'file', 'A picture or drawing of your art car.');
-
 $page->add_form_group($index, 'Image #3', 'image_3', 'file', 'A picture or drawing of your art car.');
 
 
