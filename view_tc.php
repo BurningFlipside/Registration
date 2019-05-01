ad989466f12471123fb2e5972423d1dfa4475fa7
Problem <pboyd04@gmail.com>
2019-05-01 20:53:21 +0000

    Update for bootstrap 4

--- view_tc.php
+++ view_tc.php
@@ -7,39 +7,31 @@ $page = new RegisterPage('Burning Flipside - Registration');
 $page->addJS('js/view_obj.js');
 
 $page->body .= '
-    <div class="form-group">
+    <div class="row">
         <label for="name" class="col-sm-2 control-label">Camp Name:</label>
         <div class="col-sm-10">
             <input class="form-control" type="text" name="name" id="name" readonly/>
         </div>
-    </div>
-    <div class="clearfix visible-sm visible-md visible-lg"></div>
-    <div class="form-group">
+        <div class="w-100"></div>
         <label for="logo" class="col-sm-2 control-label">Camp Logo:</label>
         <div class="col-sm-10" id="logo">
         </div>
-    </div>
-    <div class="clearfix visible-sm visible-md visible-lg"></div>
-    <div class="form-group">
+        <div class="w-100"></div>
         <label for="site" class="col-sm-2 control-label">Camp Website:</label>
         <div class="col-sm-10" id="site">
         </div>
-    </div>
-    <div class="clearfix visible-sm visible-md visible-lg"></div>
-    <div class="form-group">
+        <div class="w-100"></div>
         <label for="teaser" class="col-sm-2 control-label">One Line Teaser:</label>
         <div class="col-sm-10">
             <input class="form-control" type="text" name="teaser" id="teaser" readonly/>
         </div>
-    </div>
-    <div class="clearfix visible-sm visible-md visible-lg"></div>
-    <div class="form-group">
+        <div class="w-100"></div>
         <label for="description" class="col-sm-2 control-label">Description:</label>
         <div class="col-sm-10">
             <textarea class="form-control" rows="6" name="description" id="description" readonly></textarea>
         </div>
     </div>
-    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#contact_dialog">Contact the camp lead</button>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#contact_dialog">Contact the camp lead</button>
 </div>
 <div id="contact_dialog" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true" aria-labelledby="contact-title">
     <div class="modal-dialog">
@@ -49,13 +41,11 @@ $page->body .= '
             </div>
             <div class="modal-body">
                 <input type="hidden" id="_id" name="_id"/>
-                <div class="form-group">
+                <div class="row">
                      <label class="control-label" for="subject">Subject:</label>
                      <div class="clearfix visible-sm visible-md visible-lg"></div>
                      <input class="form-control" type="text" name="subject" id="subject"/>
-                </div>
-                <div class="clearfix visible-sm visible-md visible-lg"></div>
-                <div class="form-group">
+                     <div class="w-100"></div>
                     <label class="control-label" for="email_text">Content:</label>
                     <div class="clearfix visible-sm visible-md visible-lg"></div>
                     <textarea class="form-control" rows="6" name="email_text" id="email_text" style="margin: 0;"></textarea>
